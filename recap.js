8d2660310130303f663377a8188a5e4e241cc8aa
hosain1234 <fahim7787@gmail.com>
2022-02-24 16:03:34 +0600

    Question added. I will solve all problems after my varsity class

---
+++ recap.js
@@ -0,0 +1,18 @@
+/* 
+1. let, const use and decliar
+2. template string
+    a. use variable
+    b. use object property
+3. arrow function use and decliar every version
+    a. with one parameter. will return the number after dividing by 5
+    b. with two parameters. will add 2 to each number then multiply the result
+    c. three parameters. will multiply all three parameters
+    d. two parameter but it will be multiline arrow function. will add 2 to each number and multiply the results. Make sure it returns the result.
+4. function decliaration vs arrow function different.
+5. you have array of Numbers. map it go get elemrnt multiplied by 5 and must use arrow function.
+6. [challenginbg] Numbers of array theke get odd number by using filter with arrow function.
+7. array of objects(products). Use find to get the object which price is 5000.
+8. You have an object. Now use destructuring to create a simple variable of any property of the object. 
+9.[challenge] You have an array.Now use destructuring to create a simple(or more) variable to get the third element of the array in a variable called 'three'.
+10. [Optional] Just write a function with 3 parameter and the last parameter will have default value 7. these function will do sum of all the three parameter.
+*/
